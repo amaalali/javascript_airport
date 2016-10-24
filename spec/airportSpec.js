0172a8c7d1101066b96f024fe0d4c5dd49484b9a
antonyCastineiras <antonycastineiras80@gmail.com>
2016-10-24 16:06:20 +0100

    added takeOff function for airport

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -3,9 +3,21 @@ describe("airport", function() {
 	describe('Land', function() {
 		it('instructs plane to', function() {
 			var airport = new Airport();
-			expect(airport.land()).toEqual("Plane landed")
+			var plane = new Plane();
+			expect(airport.land(plane)).toEqual("Plane landed")
+			expect(airport.landedPlanes.length).toBe(1)
 		});
 	});
 
+	describe('takeOff', function() {
+		it('instructs a plane to take off', function() {
+			var airport = new Airport();
+			var plane = new Plane();
+			airport.land(plane)
+			landedPlanes = airport.landedPlanes.length
+			airport.takeOff(plane)
+			expect(airport.landedPlanes.length).toBe(0)
+		});
+	});
 
 })
--- src/airport.js
+++ src/airport.js
@@ -2,6 +2,13 @@
 function Airport() {
 }
 
-Airport.prototype.land = function() {
+Airport.prototype.landedPlanes = []
+
+Airport.prototype.land = function(plane) {
+	Airport.prototype.landedPlanes.push(plane)
 	return "Plane landed"
+}
+
+Airport.prototype.takeOff = function(plane) {
+	Airport.prototype.landedPlanes.splice(plane)
 }
